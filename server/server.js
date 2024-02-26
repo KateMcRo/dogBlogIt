62b94ccdbdb936ca54ad9693896745c367f5ecb3
KateMcRo <katemcr0@gmail.com>
2024-02-26 12:14:34 -0800

    added CORS middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const API = require("./API");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = 3001;
 
+// Middleware
+
+app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/", API);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Listening on port ${PORT}`);
+  console.log(`🚀 CORS-enabled web server listening on port ${PORT}`);
 });
