974b10496f4812351ae5b697c7a6f9ee1f3e67b2
KateMcRo <katemcr0@gmail.com>
2024-03-05 11:52:12 -0800

    created basic user creation route

--- server/API/userRoutes/index.js
+++ server/API/userRoutes/index.js
@@ -1,7 +1,26 @@
+const User = require("../../models/User");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.send(`👤`);
+});
+
+router.post("/create", async (req, res) => {
+  const data = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    username: data.username,
+    email: data.email,
+  };
+  res.json({ newUser });
 });
 
 module.exports = router;
