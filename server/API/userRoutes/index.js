b20e1d1e66f12d19fbed302958c87455ee1d7893
KateMcRo <katemcr0@gmail.com>
2024-03-05 13:19:29 -0800

    added log in route with error handling

--- server/API/userRoutes/index.js
+++ server/API/userRoutes/index.js
@@ -5,10 +5,12 @@ const bcrypt = require("bcrypt");
 
 require("dotenv").config();
 
+/* Route Testing */
 router.get("/", (req, res) => {
   res.send(`👤`);
 });
 
+/* Create User */
 router.post("/create", async (req, res) => {
   const data = await User.create({
     firstName: req.body.firstName,
@@ -17,6 +19,7 @@ router.post("/create", async (req, res) => {
     email: req.body.email,
     password: req.body.password,
   });
+
   const newUser = {
     firstName: data.firstName,
     lastName: data.lastName,
@@ -28,9 +31,47 @@ router.post("/create", async (req, res) => {
     expiresIn: "2h",
   });
 
-  console.log({ token });
-
   res.json({ newUser, token });
 });
+
+/* Log User In */
+try {
+  router.post("/login", async (req, res) => {
+    // Check for User by username
+    const user = await User.findOne({ username: req.body.username });
+    // Error if not found
+    if (!user) {
+      return res.json({ message: "You lost bro? Username not found." });
+    }
+    // If found (check password)
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    // wrong password (error)
+    if (!validPass) {
+      return res.json({ message: "Nice try, imposter." });
+    }
+    // Valid password (create user object)
+    const userData = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      email: user.email,
+      posts: user.posts,
+    };
+    // Sign token, send response
+    const token = jwt.sign({ userData }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+
+    res.json({ userData, token });
+  });
+} catch (error) {
+  res.json({ error: error.message });
+}
+
+/* Check for Valid Token */
+/* Log User Out */
+/* Edit User Details */
+/* Delete User */
 
 module.exports = router;
